cbdedabb5d1c08816b7a46155a8e23aac724f97a
PACI <emkqson@outlook.com>
2022-03-10 16:45:29 +0800

    Create autoupload_telebot.sh
    add telebot push msg function

---
+++ autoupload_telebot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rcloneDrive='OD:/Download/'
+downloadPath='/home/download'
+
+TOKEN=
+CHATID=
+
+if [ $2 -eq 0 ]; then
+  exit 0
+elif [ $2 -eq 1 ]; then
+  basenameStr=`basename "$3"`
+  su - -c "rclone move \"$3\" $rcloneDrive"
+  curl -s -o /dev/null "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$CHATID&text=$3 Uploaded"
+  exit 0
+else
+  filePath=$3
+  while true; do
+    dirnameStr=`dirname "$filePath"`
+    if [ "$dirnameStr" = "$downloadPath" ]; then
+      basenameStr=`basename "$filePath"`
+      su - -c "rclone move \"$filePath\" $rcloneDrive\"$basenameStr\""
+      curl -s -o /dev/null "https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$CHATID&text=$filePath Uploaded"
+      rm -r -f "$filePath"
+      exit 0
+    elif [ "$dirnameStr" = "/" ]; then
+      exit 0
+    else
+      filePath=$dirnameStr
+    fi
+  done
+fi
